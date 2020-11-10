91af24a7dde72a3d10d55914b288823b31ea231b
Philip Gray <pcgrayiii@icloud.com>
2020-11-09 21:11:36 -0800

    link masker

---
+++ masker/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	//Waiting for terminal input
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("Bruh, wheres the input?")
+		return
+	}
+	const (
+		//future note. Should I bother with https? Hm.
+		link  = "http://"
+		nlink = len(link)
+		mask  = '*'
+	)
+
+	var (
+		text = args[0]
+		size = len(text)
+		// buff = make([]byte, 0, size)
+		buff = []byte(text)
+
+		in bool
+	)
+
+	//Often we use a for range loop but for this
+	//we want to loop over the bytes of the text
+	//not over its runes.
+	for i := 0; i < size; i++ {
+		//len(text[i:]) means that it grabs the remaining part
+		// of the text then checks if theres more room for a
+		// link thus ensures it never slices beyond its capacity.
+		// && compares the input to see if it matches
+		//the link pattern
+		if len(text[i:]) >= nlink && text[i:i+nlink] == link {
+			in = true
+			// fmt.Printf(`text[%d : %[1]d+%d] = `, i, nlink)
+			// fmt.Println(text[i : i+nlink])
+			//
+
+			// buff = append(buff, link...)
+
+			//since we use the above line twice we end up adding
+			//link twice to the string
+			//so the loop jumps after the link pattern
+			//duplication
+			i += nlink
+		}
+		c := text[i]
+
+		switch c {
+		case ' ', '\t', '\n':
+			in = false
+		}
+		if in {
+			buff[i] = mask
+			// c = mask
+		}
+		// buff = append(buff, c)
+	}
+
+	fmt.Println(string(buff))
+
+}
--- write_a_file_finder/main.go
+++ write_a_file_finder/main.go
@@ -7,29 +7,33 @@ import (
 )
 
 func main() {
+	//Get the directory name and if there is no argument we ask for one and terminate the program
+	//IE: go run main.go name_of_file
 	args := os.Args[1:]
 	if len(args) == 0 {
 		fmt.Println("Provide a director")
 		return
 	}
-
+	//reads the directory path from the argument. In this program we would type go run main.go files/ and if found it will print the files out
 	files, err := ioutil.ReadDir(args[0])
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	var total int
 
+	var total int
+	//counts the file size reserving space by counting the total length of everyfile in the folder and writes the exact number of bytes to reserve.
 	for _, file := range files {
 		if file.Size() == 0 {
 			total += len(file.Name()) + 1
 		}
 	}
-
 	fmt.Printf("Total required space: %d bytes.\n", total)
 
+	//Creates an splice array of bytes
 	names := make([]byte, 0, total)
-
+	///loops through files and retrieves all empty ones.
+	//also translates files from bytes to a string via file.Name
 	for _, file := range files {
 		if file.Size() == 0 {
 			name := file.Name()
@@ -38,7 +42,8 @@ func main() {
 		}
 
 	}
-
+	//writes data to a file that takes the names parameter and writes them to a document. Creates a text file with the name of all empty text files of X directory.
+	//Unix Octal 0644 means: User Read/Write; Group Read; Other Read.
 	err = ioutil.WriteFile("out.txt", names, 0644)
 	if err != nil {
 		fmt.Println(err)
