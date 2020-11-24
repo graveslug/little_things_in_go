fbc4321065af2e89cdf4bee9c71fee2ea5223328
Philip Gray <pcgrayiii@icloud.com>
2020-11-24 08:57:27 -0800

    added a recursive solution.

--- algorithms/shape_area/shapearea.go
+++ algorithms/shape_area/shapearea.go
@@ -1,12 +1,25 @@
 package shapearea
 
+//Iterative solution
 func shapeArea(n int) int {
 	if n == 1 {
 		return n
 	}
+
 	area := 1
 
 	for i := 2; i <= n; i++ {
 		temp := ((i - 1) * 4) + area
+		area = temp
 	}
+	return
+}
+
+//Recursive solution
+func shapeArea2(n int) int {
+	if n == 1 {
+		return n
+	}
+
+	return shapeArea(n-1) + (4 * (n - 1))
 }
