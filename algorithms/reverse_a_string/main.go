76bb67b03f81c9ac797969fd33bd545ce828d8b3
Philip Gray <pcgrayiii@icloud.com>
2020-11-28 10:54:48 -0800

    reversed a string using Go runes

---
+++ algorithms/reverse_a_string/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func reverseAString(str string) string {
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func main() {
+	input := "yeah, I guess I'm 死"
+	fmt.Println(reverseAString(input))
+}
