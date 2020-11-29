16f75019dd7be05a41a99d5c6f07ce6307bd4982
Philip Gray <pcgrayiii@icloud.com>
2020-11-28 18:03:45 -0800

    added inceasing sequence

---
+++ algorithms/increasing_sequence/main.go
@@ -0,0 +1,28 @@
+package main
+
+func increasingSequence(sequence []int) bool {
+	c1 := 0
+	c2 := 0
+	for i := 0; i < len(sequence)-1; i++ {
+		if sequence[i] >= sequence[i+1] {
+			c1++
+		}
+	}
+	for i := 0; i < len(sequence)-2; i++ {
+		if sequence[i] >= sequence[i+2] {
+			c2++
+		}
+	}
+	return (c1 <= 1) && (c2 <= 1)
+}
+
+func main() {
+	input := []int{1, 2, 5, 4}              //true
+	input2 := []int{1, 1}                   //true
+	input3 := []int{1, 2, 1, 2}             //false
+	input4 := []int{3, 6, 5, 8, 10, 20, 15} //false
+	increasingSequence(input)
+	increasingSequence(input2)
+	increasingSequence(input3)
+	increasingSequence(input4)
+}
---
+++ algorithms/increasing_sequence/readme.md
@@ -0,0 +1,16 @@
+# increasing sequence
+
+Given a sequence of integers as an array we must determine whether if it is possible to obtain an increasing sequence by removing one or less elements from an array. Return value should be a boolean.
+
+Notes:
+it does not have to be 1,2,3,4,5.
+it can be 10, 20, 30. The goal is that it is increasing only.
+
+input = [1,1]
+output = true
+
+input = [1,2,6,7,5]
+output = true
+
+input = [1,1,3,5,8,8]
+output = false
